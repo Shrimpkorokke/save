53ec7539278521a9d3dadca5f92104b75fdf24c0
Shrimpkorokke <101083615+Shrimpkorokke@users.noreply.github.com>
2022-03-30 21:33:43 +0900

    Minimap and warp

---
+++ Scripts/JK_MiniMap.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JK_MiniMap : MonoBehaviour
+{
+    public Transform playerIcon;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "Warrior")
+        {
+            playerIcon.transform.position = transform.position + new Vector3(0, 20, 0);
+            print("gg");
+        }
+    }
+}
